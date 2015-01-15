af3ee3555b12d80c60e9be7c850090cc7950ca5b
Cray Elliott <MP2E@archlinux.us>
2015-01-15 01:32:30 -0800

    add uptime command, cleanup a bit, rename to divebot
    divebot is a reference to Diveman from Rockman 4 or Mega Man IV in the US :P

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ import Data.List
 import Network
 import System.IO
 import System.Exit
+import System.Time
 import Control.Arrow
 import Control.Monad.Reader
 import Control.Exception
@@ -10,11 +11,11 @@ import Text.Printf
 server = "irc.oftc.net"
 port   = 6667
 chan   = "#mp2e-testing"
-nick   = "tutbot"
+nick   = "divebot"
 
 -- The 'Net' monad, a wrapper over IO, carrying the bot's immutable state.
 type Net = ReaderT Bot IO
-data Bot = Bot { socket :: Handle }
+data Bot = Bot { socket :: Handle, starttime :: ClockTime }
 
 -- Set up actions to run on start and end, and run the main loop
 main :: IO ()
@@ -26,16 +27,16 @@ main = bracket connect disconnect loop
 -- Connect to the server and return the initial bot state
 connect :: IO Bot
 connect = notify $ do
+    t <- getClockTime
     h <- connectTo server (PortNumber (fromIntegral port))
     hSetBuffering h NoBuffering
-    return (Bot h)
+    return (Bot h t)
   where
     notify a = bracket_
         (printf "Connecting to %s ... " server >> hFlush stdout)
         (putStrLn "done.")
         a
 
--- We're in the Net monad now, so we've connected successfully
 -- Join a channel, and start processing commands
 run :: Net ()
 run = do
@@ -59,8 +60,27 @@ listen h = forever $ do
 -- Dispatch a command
 eval :: String -> Net ()
 eval     "!quit"               = write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
+eval     "!uptime"             = uptime >>= privmsg
 eval x | "!id " `isPrefixOf` x = privmsg (drop 4 x)
-eval     _                    = return () -- ignore everything else
+eval     _                     = return () -- ignore everything else
+
+
+uptime :: Net String
+uptime = do
+    now  <- io getClockTime
+    zero <- asks starttime
+    return . pretty $ diffClockTimes now zero
+
+-- Pretty print the date in '1d 9h 9m 17s' format
+pretty :: TimeDiff -> String
+pretty td =
+  unwords $ map (uncurry (++) . first show) $
+  if null diffs then [(0,"s")] else diffs
+  where merge (tot,acc) (sec,typ) = let (sec',tot') = divMod tot sec
+                                    in (tot',(sec',typ):acc)
+        metrics = [(86400,"d"),(3600,"h"),(60,"m"),(1,"s")]
+        diffs = filter ((/= 0) . fst) $ reverse $ snd $
+                foldl' merge (tdSec td,[]) metrics
 
 -- Send a privmsg to the current chan + server
 privmsg :: String -> Net ()
